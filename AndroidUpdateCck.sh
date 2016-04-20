736f166e469021ed70657d1cd2d2028233a92b44
carcheky <carcheky@gmail.com>
2016-04-20 15:42:32 +0200

    some fixes

--- AndroidUpdateCck.sh
+++ AndroidUpdateCck.sh
@@ -1,8 +1,9 @@
 echo "-------> reboot bootloader"
 adb reboot-bootloader
+
 echo "-------> preparando imagen"
-rm -fr FactoryImage/update
 cd FactoryImage
+  rm -fr update
   mkdir update
   tar -xvf hammerhead*.tgz -C update
   cd update
@@ -12,8 +13,8 @@ cd FactoryImage
     rm image-hammerhead-*.zip
   cd ..
 cd ..
+
 echo "-------> instalando actualizacion"
-adb reboot bootloader
 sleep 5
 fastboot flash bootloader FactoryImage/update/bootloader-hammerhead-*.img
 fastboot reboot-bootloader
@@ -22,16 +23,18 @@ fastboot flash radio FactoryImage/update/radio-hammerhead-*.img
 fastboot reboot-bootloader
 sleep 5
 fastboot flash boot FactoryImage/update/boot.img
+fastboot reboot-bootloader
 sleep 5
 fastboot flash cache FactoryImage/update/cache.img
+fastboot reboot-bootloader
 sleep 5
 fastboot flash system FactoryImage/update/system.img
+fastboot reboot-bootloader
 sleep 5
 fastboot reboot-bootloader
 sleep 5
 fastboot flash recovery multirom/TWRP_multirom_hammerhead_*.img
-sleep 5
 fastboot reboot-bootloader
+
 echo "-------> borrando archivos temporales"
 rm -fr FactoryImage/update
-sleep 5
